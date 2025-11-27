77d187523e88e2682dfad2820ef02b133f6622e1
Bernapai <juanemeunier@gmail.com>
2025-11-27 16:39:07 -0300

    feat: Add middleare

---
+++ src/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const autenticar = (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+
+        if (!token) {
+            return res.status(401).json({
+                error: 'Token no proporcionado'
+            });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'tu_clave_secreta');
+        req.usuario = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            error: 'Token inválido'
+        });
+    }
+};
+
+module.exports = {
+    autenticar
+};
