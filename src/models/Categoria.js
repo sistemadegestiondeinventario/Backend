8a3a577a704958802e1b83378d50c3d7fc6ac6ff
Bernapai <juanemeunier@gmail.com>
2025-11-27 16:11:42 -0300

    feat: Modelo Categoria

---
+++ src/models/Categoria.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Categoria = sequelize.define('Categoria', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nombre: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true
+    },
+    descripcion: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    }
+}, {
+    timestamps: true,
+    createdAt: 'fecha_creacion',
+    updatedAt: false,
+    tableName: 'categorias'
+});
+
+module.exports = Categoria;
